92e5827797ccfd6b0d2dca935fcdd7c9de850461
Tanner Festa <festat1@newpaltz.edu>
2023-05-10 09:44:38 -0400

    Removed Implicit Deprecated Event

--- server/api/products.ts
+++ server/api/products.ts
@@ -1,13 +1,14 @@
 let productCount = 0;
 
-export default async () => {
-    await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            productCount++;
-            resolve(true)
-        }, 1000)
-    });
-    return {
-        productCount,
-    }
+function handleEvent() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      productCount++;
+      resolve(true);
+    }, 1000);
+  }).then(() => ({
+    productCount,
+  }));
 }
+
+export default handleEvent;

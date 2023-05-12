899f68c580b88d7f810db4d74b01ba3a5006b2ff
Tanner Festa <festat1@newpaltz.edu>
2023-05-12 12:42:42 -0400

    Dynamic Loading Mechanism

--- pages/weeklong/index.vue
+++ pages/weeklong/index.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
-    const users = ref<User[]>([]);
     import { User } from "types"
+    import axios from 'axios';
+
+    const users = ref<User[]>([]);
+    const usersLoaded = ref(false);
+    
     useHead({
         title: "Weeklong",
     })
@@ -10,21 +14,63 @@
                                 ["John Doe", "25", "johndoe@example.com", "johndoe@example.com", "YES"],
                                 ["John Doe", "25", "johndoe@example.com", "johndoe@example.com", "YES"]]
 
-    const { data: items, pending } = await useLazyAsyncData("users", () => $fetch('api/models/users'))
     // const users: User[] = items as User[];
     // console.log(products.value)
     // console.log(items.value.data)
+
+    // async function getUserValue(){
+    onMounted(async () => {
+      try {
+        const response = await axios.get('api/models/users');
+        const items = response.data.data;
+        // console.log(items);
+
+        users.value = items.map((item : User) => ({
+          photo: item.photo,
+          nickname: item.nickname,
+          team: item.team,
+          tags: item.tags,
+          daysSurvived: item.daysSurvived,
+          mod: item.isMod,
+        }));
+
+        console.log(users.value);
+        usersLoaded.value = true;
+      } catch (error) {
+        console.error(error);
+      }
+    });
+
+    //     // users.value = items.value?.data.map((item: any) => 
+    //     // ({
+    //     //     photo: item.photo,
+    //     //     nickname: item.nickname,
+    //     //     team: item.team,
+    //     //     tags: item.tags,
+    //     //     daysSurvived: item.daysSurvived,
+    //     //     mod: item.isMod
+    //     // }));
+    //     console.log(users.value)
+
+    // }
+
+    // onMounted(async () => {
+    //     const { data: items, pending } = await useLazyFetch('api/models/users',
+    //     {
+    //     // just get the data, and not the array infront of it.
+    //         transform: (_items) => _items.data, 
+    //     }).then((res) => {
+    //         console.log(res.data)
+    //         // console.log(items)
+    //         // productsShow.value = res
+    //         return res
+    //     })
+    //     // console.log(items.value)
+    // })
     
 
     // const users: User[] = (items.value.data as unknown) as User[];
-    users.value = items.value?.data.map((item: any) => ({
-        photo: item.photo,
-        nickname: item.nickname,
-        team: item.team,
-        tags: item.tags,
-        daysSurvived: item.daysSurvived,
-        mod: item.isMod
-    }));
+    
     // onBeforeMount(async () => {
     //     const { data: items, pending } = await useLazyAsyncData("users", () => $fetch('api/models/users'))
 
@@ -42,7 +88,7 @@
     // });
     const refresh = () => refreshNuxtData("users")
 
-    console.log(users.value)
+    // console.log(users.value)
 
 
     
@@ -54,7 +100,9 @@
 
 <template>
     <div>
-        <Table :headers="headers" :data="users"/>
+        <Table v-if="usersLoaded" :headers="headers" :data="users" />
+        <div v-else>Loading...</div>
+        
 
     </div>
 </template>

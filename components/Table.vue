59a86f559aa39e030069b31762cc18d42b8a7dfc
Tanner Festa <festat1@newpaltz.edu>
2023-05-15 18:25:14 -0400

    Bad UI/Good Backend for Plr Requesting

--- components/Table.vue
+++ components/Table.vue
@@ -8,10 +8,26 @@
             type: Array,
             required: true,
         },
+        displayAdminTable: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        isAdmin: {
+            type: Boolean,
+            required: false,
+            default: false,
+        }
     });
     // Destructure the props to access them
-    const { headers, data } = props;
-    refreshNuxtData("users")
+    const { headers, data, displayAdminTable, isAdmin } = props;
+
+    if (displayAdminTable && isAdmin)
+    {
+        headers.push("Allow")
+        headers.push("Deny")
+    }
+    // refreshNuxtData("users")
     // console.log(headers)
 
 </script>
@@ -26,15 +42,33 @@
             </thead>
             <tbody>
                 <tr v-for="item in data">
-                    <td v-for="i in item" class="border px-4 py-2">{{ i }}</td>
+                    <td v-for="(data, index) in item" class="border px-4 py-2">
+                        {{ data }}
+                        <!-- {{ index }} -->
+                        <!-- <div v-if="index=='photo' && displayAdminTable && isAdmin" >
+                            hi
+                        </div> -->
+                    
+                    </td>
+                    <td v-if="displayAdminTable && isAdmin" class="border px-4 py-2 buttonNoStyling" style="background-color:green" >
+                           
+                           
+                    </td>
+                    <td v-if="displayAdminTable && isAdmin" class="border px-4 py-2 buttonNoStyling" style="background-color:rgb(117, 39, 39)" >
+                           
+                           
+                    </td>
+                    
                 </tr>
+                
             
             </tbody>
         </table>
+
+        
+
     </div>
 </template>
-
-
 
 <style scoped>
 
--- pages/weeklong/requestPlrsTable.vue
+++ pages/weeklong/requestPlrsTable.vue
@@ -1,13 +1,54 @@
 <script setup lang="ts">
+    import { User } from "types"
+    import axios from 'axios';
     useHead({
         title: "Requested Players",
     })
+
+    const users = ref<User[]>([]);
+    const usersLoaded = ref(false);
+
+    const isAdmin = ref(false);
+
+
+
+    const headers : string[] = ["Photo", "Name", "Mod"]
+    onMounted(async () => {
+      try {
+        const response = await axios.get('../api/models/users');
+        const items = response.data.data;
+        // console.log(items);
+
+        users.value = items
+          .filter((item: User) => !item.isInWeeklong && item.requestingWeeklong)
+          .map((item : User) => ({
+            
+            photo: item.photo,
+            nickname: item.nickname,
+            mod: item.isMod,
+        }));
+
+        isAdmin.value = items.filter((item: User) => item.isAdmin && item.email === useSupabaseUser().value?.email).length > 0;
+        console.log(isAdmin.value, "isAdmin")
+
+        
+        usersLoaded.value = true;
+      } catch (error) {
+        console.error(error);
+      }
+
+
+    });
 
 </script>
 
 <template>
     <div>
-        <p>hello</p>
+        <div v-if="usersLoaded">
+            <Table :headers="headers" :data="users" :displayAdminTable="true" :isAdmin="isAdmin" />
+        </div>
+        <div v-else class="bg-gray-200 h-4 w-1/2 animate-pulse rounded-lg"></div> <!-- Loading bar -->
+
     </div>
 </template>
 

eb622a8c5b25e22558aeab427f9ffdba598718f6
Tanner Festa <festat1@newpaltz.edu>
2023-05-29 11:04:27 -0400

    Styled Events Page

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -9,29 +9,41 @@
     const infoEvents = ref([
         {
             header: "Hello",
-            content: "This is the content"
+            content: "This is the content",
+            isShown: false,
         },
         {
             header: "Hello2",
-            content: "This is the content2"
+            content: "This is the content2",
+            isShown: false,
         }
-    ]) as Ref<{header: string, content: string}[]>
+    ]) as Ref<{header: string, content: string, isShown: boolean}[]>
     
 
 </script>
 
 
 <template>
-    <div class="Main">
-        <!-- <Alert/>
-        <Profile/> -->
-        <ProfileHeaderAvatar/>
-        <div v-for="(event, index) in infoEvents">
-            {{ event.header }}
-            <IconsArrowDown class="svgImage"/>
-
+    <div class="grid grid-cols-1 gap-4">
+    <!-- <Alert/>
+    <Profile/> -->
+    <!-- <ProfileHeaderAvatar/> -->
+    <div v-for="(event, index) in infoEvents" :key="index" class="bg-white p-4 rounded-md shadow">
+        <div class="flex items-center justify-center mb-2">
+        <div class="text-lg font-semibold text-center text-black">{{ event.header }}</div>
+        <div v-if="!event.isShown">
+            <IconsArrowDown class="svgImage text-black" @click="infoEvents[index].isShown = true"/>
         </div>
-        <!-- <img src="~/assets/images/down-arrow.png" alt="Down Arrow" /> -->
-        
+        <div v-else>
+            <IconsArrowUp class="svgImage text-black" @click="infoEvents[index].isShown = false"/>
+        </div>
+        </div>
+        <div v-if="event.isShown" class="mt-4 text-black">
+            {{ event.content }}
+        </div>
     </div>
+    <!-- <img src="~/assets/images/down-arrow.png" alt="Down Arrow" /> -->
+    </div>
+
+
 </template>

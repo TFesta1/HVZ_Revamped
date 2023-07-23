2d21ea96858019caafd263f35e7809aa5056b773
Tanner Festa <festat1@newpaltz.edu>
2023-07-23 17:07:25 -0400

    midnightInc for Netlify

--- midnightIncDays.bat
+++ midnightIncDays.bat
@@ -1,7 +1,8 @@
 @echo off
 
 REM Replace the API URL with your actual API endpoint
-set "apiUrl=http://localhost:3000/api/models/midnightIncDays"
+REM set "apiUrl=http://localhost:3000/api/models/midnightIncDays"
+set "apiUrl=https://newpaltzhvz.netlify.app/api/models/midnightIncDays"
 
 REM Call the API using curl
 curl %apiUrl%
